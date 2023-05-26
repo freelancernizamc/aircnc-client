771d7e26ac760fd969b7542e621d9e12b7276209
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-05-26 22:05:45 +0600

    first commit

---
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Container from '../Container';
+
+const Navbar = () => {
+    return (
+        <div className='fixed w-full bg-white z-10 shadow-sm'>
+            <div className='py-4 border-b-[1px]'></div>
+            <Container>
+
+                <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
+                    <div>Logo</div>
+                    <div>Search</div>
+                    <div>Menu</div>
+                </div>
+            </Container>
+
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAURBHRvxFEQooMLZ_JJpmDgpfK2AZ_sZg",
+  authDomain: "aircnc-client-5846e.firebaseapp.com",
+  projectId: "aircnc-client-5846e",
+  storageBucket: "aircnc-client-5846e.appspot.com",
+  messagingSenderId: "591851407445",
+  appId: "1:591851407445:web:1473085f0b5fc1f960ecdc"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
