feed7893b05dd635373a2b6faf3f284d69ea6fd2
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-05-27 00:14:26 +0600

    Navbar completed

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Container from '../Container';
+import Logo from './Logo';
+import Search from './Search';
+import MenuDropdown from './MenuDropDown';
 
 const Navbar = () => {
     return (
@@ -8,9 +11,9 @@ const Navbar = () => {
             <Container>
 
                 <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
-                    <div>Logo</div>
-                    <div>Search</div>
-                    <div>Menu</div>
+                    <Logo />
+                    <Search />
+                    <MenuDropdown />
                 </div>
             </Container>
 
